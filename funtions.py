8fa338e25bc058aa78530dcee891d1192d4aa693
jcarrenoa <jcarrenoa@uninorte.edu.co>
2024-07-27 15:40:56 -0500

    requirements

--- funtions.py
+++ funtions.py
@@ -4,12 +4,12 @@ import os
 class functions():
 
     def empresas_competidoras(self):
-        return f"Las empresas competidoras son: {self.get_data_from_db()}"
+        return f"{self.get_data_from_db()}"
     
     def informacion_no_disponible(self):
         return "Informacion no disponible"
 
-    def get_data_from_db(self):
+    def get_data_from_db(self, ventas_anuales = None):
         conn = mysql.connector.connect(
             host="viaduct.proxy.rlwy.net",
             user="root",
@@ -17,8 +17,9 @@ class functions():
             database="railway",
             port=25264
         )
-        print(conn.is_connected())
         cursor = conn.cursor(dictionary=True)
+        if (ventas_anuales):
+            query = f"SELECT * FROM empresas_comp WHERE ventas_anuales >= {ventas_anuales}"
         query = f"SELECT * FROM empresas_comp"
         cursor.execute(query)
         result = cursor.fetchall()
